31140a024a5d12e670038885fd75ee147d8a8bab
Saptarshi Bhattacherya <bhat.bab@gmail.com>
2020-05-11 18:57:24 -0600

    initial commit

---
+++ template.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define print(x) cout << #x << ": " << x << endl
+
+using namespace std;
+typedef long long ll;
+
+void red8() {
+    cout << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int T;
+    cin >> T;
+    for(int i = 0; i < T; ++i) {
+        cout << "Case #" << i + 1 << ": ";
+        red8();
+    }
+
+    return 0;
+}
