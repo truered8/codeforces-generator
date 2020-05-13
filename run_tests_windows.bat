78d93b0826c9336e86fa155700f6d3cc6a04ee5c
Saptarshi Bhattacherya <bhat.bab@gmail.com>
2020-05-12 23:32:56 -0600

    added temp directory

--- run_tests_windows.bat
+++ run_tests_windows.bat
@@ -1,10 +1,12 @@
 @echo off
+echo Compiling program...
 g++ {{SOLUTION_NAME}} -o output.exe
+echo Compilation finished.
 if "{{SAMPLE_LEN}}"=="0" (
     output < {{SAMPLE_DIR_NAME}}\input0.txt > output0.txt
     fc output0.txt {{SAMPLE_DIR_NAME}}\output0.txt > nul
     if errorlevel 1 (
-        echo Failed test 0
+        echo Failed test 0 :^^O
         echo Expected output:
         type {{SAMPLE_DIR_NAME}}\output0.txt
         echo.
@@ -12,24 +14,25 @@ if "{{SAMPLE_LEN}}"=="0" (
         type output0.txt
         echo.
     ) else (
-        echo Passed test 0
+        echo Passed test 0 :^^D
     )
     del output0.txt
 ) else (
+    if not exist {{TEMP_DIR_NAME}} mkdir {{TEMP_DIR_NAME}}
     for /L %%i in (0, 1, {{SAMPLE_LEN}}) do (
-        output < {{SAMPLE_DIR_NAME}}\input%%~ni.txt > output%%~ni.txt
-        fc output%%~ni.txt {{SAMPLE_DIR_NAME}}\output%%~ni.txt > nul
+        output < {{SAMPLE_DIR_NAME}}\input%%~ni.txt > {{TEMP_DIR_NAME}}\output%%~ni.txt
+        fc {{SAMPLE_DIR_NAME}}\output%%~ni.txt {{TEMP_DIR_NAME}}\output%%~ni.txt > nul
         if errorlevel 1 (
-            echo Failed test %%i
+            echo Failed test %%i :^^O
             echo Expected:
             type {{SAMPLE_DIR_NAME}}\output%%~ni.txt
             echo.
             echo Recieved:
-            type output%%~ni.txt
+            type {{TEMP_DIR_NAME}}\output%%~ni.txt
             echo.
         ) else (
-            echo Passed test %%i
+            echo Passed test %%i :^^D
         )
-        del output%%~ni.txt
+        del {{TEMP_DIR_NAME}}\output%%~ni.txt
     )
 )
