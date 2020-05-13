a461e4416feab751c7bce649d5cc6ffd6064feaf
Saptarshi Bhattacherya <bhat.bab@gmail.com>
2020-05-12 23:32:03 -0600

    added config.json

--- generate.js
+++ generate.js
@@ -2,12 +2,17 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 const path = require('path');
 const fs = require('fs');
+const config = require('./config.json');
 
-const PROBLEMSET_URL = 'http://codeforces.com/problemset/problem'; // The URL to all problems
-const PROBLEM_DIR = '../'; // The path where every problem directory will be saved
-const TEMPLATE_PATH = './template.cpp'; // The path to the master template file
-const SOLUTION_NAME = 'solution.cpp'; // The name of the template file created in each problem directory
-const SAMPLE_DIR_NAME = 'samples'; // The name of the test sample directory
+/** The URL to all problems */
+const PROBLEMSET_URL = 'http://codeforces.com/problemset/problem';
+
+/** Configuration variables from ./config.json */
+const PROBLEM_DIR = config.PROBLEM_DIR; // The path where every problem directory will be saved
+const TEMPLATE_PATH = config.TEMPLATE_PATH; // The path to the master template file
+const SOLUTION_NAME = config.SOLUTION_NAME; // The name of the template file created in each problem directory
+const SAMPLE_DIR_NAME = config.SAMPLE_DIR_NAME; // The name of the test sample directory
+const TEMP_DIR_NAME = config.TEMP_DIR_NAME; // The name of the temporary directory created for testing
 
 /** 
  * This function is used to get the HTML content of any webpage.
@@ -53,13 +58,14 @@ const getSamplesFromURL = async url => {
  * @param {number} sampleLen - The number of test cases
  */
 const generateRunTests = (problemDirectory, sampleLen) => {
-    const runFilePath = path.join(problemDirectory, 'runtests_windows.bat');
-    fs.copyFileSync('./runtests_windows.bat', runFilePath);
+    const runFilePath = path.join(problemDirectory, 'run_tests_windows.bat');
+    fs.copyFileSync('./run_tests_windows.bat', runFilePath);
     fs.readFile(runFilePath, 'utf8', (err, data) => {
         if (err) return console.log(err);
         const result = data
             .replace(/{{SOLUTION_NAME}}/g, SOLUTION_NAME)
             .replace(/{{SAMPLE_DIR_NAME}}/g, SAMPLE_DIR_NAME)
+            .replace(/{{TEMP_DIR_NAME}}/g, TEMP_DIR_NAME)
             .replace(/{{SAMPLE_LEN}}/g, sampleLen);
         fs.writeFile(runFilePath, result, 'utf8', err => {
             if (err) return console.log(err);
@@ -68,7 +74,7 @@ const generateRunTests = (problemDirectory, sampleLen) => {
 }
 
 /**
- * This function generates the directory containing all test cases
+ * This function generates the directory containing all test cases.
  * @param {Object[]} samples - The inputs and outputs of each test case
  * @param {string} sampleDirectory - The directory in which the test cases will be saved
  */
@@ -88,20 +94,22 @@ const generateSamples = (samples, sampleDirectory) => {
 
 /**
  * This function creates the problem directory with all of the needed files.
- * @param {string} problemName - The name of the problem
+ * @param {string} problemCode - The code of the problem
  */
-const generateProblemDirectory = async problemName => {
-    const samples = await getSamplesFromURL(`${PROBLEMSET_URL}/${problemName.slice(0, -1)}/${problemName.slice(-1)}`);
-    const problemDirectory = path.join(PROBLEM_DIR, problemName)
+const generateProblemDirectory = async problemCode => {
+    const samples = await getSamplesFromURL(`${PROBLEMSET_URL}/${problemCode.slice(0, -1)}/${problemCode.slice(-1)}`);
+    const problemDirectory = path.join(PROBLEM_DIR, problemCode)
     const sampleDirectory = path.join(problemDirectory, SAMPLE_DIR_NAME)
+    const templatePath = path.join(problemDirectory, SOLUTION_NAME);
     !fs.existsSync(problemDirectory) && fs.mkdirSync(problemDirectory);
-    fs.copyFileSync(TEMPLATE_PATH, path.join(problemDirectory, SOLUTION_NAME));
+    !fs.existsSync(templatePath) && fs.copyFileSync(TEMPLATE_PATH, templatePath, fs.constants.COPYFILE_EXCL);
     generateRunTests(problemDirectory, samples.length - 1);
     generateSamples(samples, sampleDirectory);
-    console.log(`Generated problem directory for problem ${problemName}!`);
+    console.log(`Generated problem directory for problem ${problemCode}!`);
     console.log(`You can find the test cases in ${sampleDirectory}.`)
 }
 
+// If no problem names were provided
 if(process.argv.length < 3) {
     console.log('Error: couldn\'t find problem name.')
     console.log('Please try running the script in the following format: ');
@@ -109,4 +117,4 @@ if(process.argv.length < 3) {
     process.exit(1);
 }
 
-for(problemName of process.argv.slice(2)) generateProblemDirectory(problemName);
+for(problemCode of process.argv.slice(2)) generateProblemDirectory(problemCode);
